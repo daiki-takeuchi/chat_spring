0dfcf52d468e34a1d33798132c771fb2e9295a3e
daiki-takeuchi <daiki.takeuchi.0327@gmail.com>
2019-04-05 11:32:57 +0900

    DB名を微修正

--- src/main/resources/sql/00-database.sql
+++ src/main/resources/sql/00-database.sql
@@ -1,6 +1,6 @@
-create database if not exists gabandb01 character set utf8;
+create database if not exists gabandb001 character set utf8;
 create user 'chat_user'@'localhost' identified by 'chat_user';
-grant all on gabandb01.* to 'chat_user'@'localhost';
-create database if not exists gabandb01_test character set utf8;
+grant all on gabandb001.* to 'chat_user'@'localhost';
+create database if not exists gabandb001_test character set utf8;
 create user 'chat_user_test'@'localhost' identified by 'chat_user_test';
-grant all on gabandb01_test.* to 'chat_user_test'@'localhost';
+grant all on gabandb001_test.* to 'chat_user_test'@'localhost';
